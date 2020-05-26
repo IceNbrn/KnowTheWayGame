852250a3ad0e270bcea08f0e3cd387b1edc7aeb6
Carlos Castanheira <aereongames@gmail.com>
2020-05-27 00:14:19 +0100

    lasr commit

---
+++ Assets/Scripts/MusicBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBackground : MonoBehaviour
+{
+    public AudioSource introMusic, loopMusic;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        introMusic.Play();
+        
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        loopMusic.PlayDelayed(introMusic.clip.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
