de9713fdbca027fb96138ea23569c77e9eefc045
Nuno Araújo <araujonuno2000@gmail.com>
2020-05-26 02:38:40 +0100

    Door Sound

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class RotateObject : MonoBehaviour
 {
     private Transform m_ObjectToRotate;
     private bool m_IsRotated;
+    public AudioSource open;
+    public AudioSource close;
     public Transform ObjectToRotate
     {
         get { return m_ObjectToRotate; }
@@ -15,6 +18,8 @@ public class RotateObject : MonoBehaviour
     private void Start()
     {
         m_ObjectToRotate = GetComponent<Transform>();
+        open.Stop();
+        close.Stop();
     }
 
     public void Rotate(Vector3 value, PhotonView pv)
@@ -29,11 +34,13 @@ public class RotateObject : MonoBehaviour
         {
             m_IsRotated = true;
             m_ObjectToRotate.Rotate(value);
+            open.Play(0);
         }
         else
         {
             m_IsRotated = false;
             m_ObjectToRotate.Rotate(-value);
+            close.Play(0);
         }
 
     }
